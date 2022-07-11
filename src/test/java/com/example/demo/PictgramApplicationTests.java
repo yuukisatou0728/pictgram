3ff4ec40c9cff14d6c9265cf40d90cdf778a21e1
yuukisatou0728 <Frisk.pepper728@outlook.jp>
2022-07-11 12:18:51 +0900

    名前変更

--- src/test/java/com/example/demo/PictgramApplicationTests.java
+++ src/test/java/com/example/demo/PictgramApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PictogramApplicationTests {
+class PictgramApplicationTests {
 
 	@Test
 	void contextLoads() {
